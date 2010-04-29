d7f012a4cf510f740ae4e15886835dda35dc073d
ussy <ussy00@gmail.com>
2010-04-30 00:04:29 +0900

    対象のキーだけキャンセルするようにした

--- cacoo_keybind_for_sheets.user.js
+++ cacoo_keybind_for_sheets.user.js
@@ -13,11 +13,13 @@ with (unsafeWindow) {
     if (e.keyCode == 74) {
       var next = $(document.querySelector("ul.s_shapeSheetNavi a.active")).parent().next().find("a");
       next.length == 0 ? $(document.querySelector("ul.s_shapeSheetNavi li:first-child a")).click() : next.click();
+
+      e.preventDefault();
     } else if (e.keyCode == 75) {
       var prev = $(document.querySelector("ul.s_shapeSheetNavi a.active")).parent().prev().find("a");
       prev.length == 0 ? $(document.querySelector("ul.s_shapeSheetNavi li:last-child a")).click() : prev.click();
-    }
 
-    e.preventDefault();
+      e.preventDefault();
+    }
   }, false);
 }
